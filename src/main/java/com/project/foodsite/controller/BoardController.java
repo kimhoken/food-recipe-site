24b3a08e835ee7eba51251ca8ad8436898c5e5ed
seungyeon8802 <seungyeon8302@gmail.com>
2026-06-09 15:23:03 +0900

    .

--- src/main/java/com/project/foodsite/controller/BoardController.java
+++ src/main/java/com/project/foodsite/controller/BoardController.java
@@ -26,7 +26,7 @@ public class BoardController {
     public String boardList(Model model){
         List<BoardVO> list = boardDao.selectAll();
         model.addAttribute("list", list);
-        return "/board/board_list";
+        return "board/board_list";
     }
 
     //board 검색
@@ -36,14 +36,14 @@ public class BoardController {
 
         model.addAttribute("list", list);
         model.addAttribute("searchWord", search);  //검색어 보관
-        return "/board/board_list";
+        return "board/board_list";
     }
 
     //recipe 등록 폼
     @GetMapping("/regiRecipe.do")
     public String recipeForm(Model model, String id){
         model.addAttribute("id", id); 
-        return "/board/board_regiRecipe";
+        return "board/board_regiRecipe";
     }
 
     //내 레시피 등록하기
@@ -54,7 +54,12 @@ public class BoardController {
         System.out.println("등록하려는 레시피의 재료" + dto.getVegetableName().size());  
         System.out.println("등록하려는 재료의 양" + dto.getAmountV()); 
         
-        boardDao.insertRecipe(dto);
+        boardDao.insertRecipe(dto); //레시피테이블에 레시피 등록
+        /**
+         * 레시피테이블에 제목이랑 썸네일 이미지 등록 후 방금 등록한 레시피 ID가져오기
+         * 재료테이블에 재료를 넣고, 조리순서 테이블에 조리순서, 이미지를 넣어 아까 만든 레시피 ID와 연결
+         * 게시판 테이블에 레시피ID, member_id를 참조하게 하고 제목, 내용 넣기  
+         */
         
         return "redirect:/list.do";
     }
