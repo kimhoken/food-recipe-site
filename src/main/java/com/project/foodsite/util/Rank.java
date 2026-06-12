3a1b51cff96d0ad026d443ba96ec7a7f5ac71442
HANE48 <hun2me2ne@gmail.com>
2026-06-12 12:38:28 +0900

    중간 저장

---
+++ src/main/java/com/project/foodsite/util/Rank.java
@@ -0,0 +1,14 @@
+package com.project.foodsite.util;
+
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+
+import lombok.*;
+
+@Service
+public class Rank {
+    //실시간 검색어 랭킹
+    //아아 이거 어케만드냐
+    
+}
