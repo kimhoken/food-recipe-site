36cc316d051712470cbd4054ab700a7ae7bf7f3e
kimhoken <kimhk441@naver.com>
2026-06-24 11:47:36 +0900

    파일 업로드 묘둘 (여러 파일 추가 및  선택 삭제 )

--- src/main/java/com/project/foodsite/common/Fileupload.java
+++ src/main/java/com/project/foodsite/common/Fileupload.java
@@ -1,6 +1,9 @@
 package com.project.foodsite.common;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -45,7 +48,7 @@ public class Fileupload {
     }
 
 
-    //파일 제거 함수
+    // 파일 제거 함수
     public void deleteFile(String filename, String folder){
         
         if (filename == null || filename.equals("no_file.png")){
@@ -59,6 +62,77 @@ public class Fileupload {
         }
 
     }
+
+    // 여러 이미지 파일 저장 함수
+    public String saveFiles(List<MultipartFile> files, String folder) throws Exception{
+
+        List<String> filenames = new ArrayList<>();
+
+
+        if( files == null || files.isEmpty()){
+            return null;
+        } 
+
+        for(MultipartFile file : files){
+
+            if(file == null || file.isEmpty()){
+                continue;
+            }
+
+            filenames.add(saveFile(file, folder));
+
+        }
+        return String.join(",", filenames);
+
+    }
+
+    // 여러 이미지 파일 제거 함수
+    public void deleteFiles(String filenames, String folder){
+
+        if(filenames == null || filenames.isEmpty()){
+            return;
+        }
+
+        List<String> fileList = Arrays.asList(filenames.split(","));
+
+        for( String filename : fileList){
+            deleteFile(filename.trim(), folder);
+        }
+
+    }
+
+    // 파일 선택 삭제
+    public String deleteSelectFiles(String image_list, List<String> deleteList, String folder){
+
+        if(image_list == null || image_list.isEmpty()){
+            return null;
+        }
+
+        if(deleteList == null || deleteList.isEmpty()){
+            return null;
+        }
+
+        List<String> currentList = new ArrayList<>( Arrays.asList(image_list.split(",")) );
+
+        for( String deletename : deleteList ){
+
+            if(deletename == null || deletename.isEmpty()){
+                continue;
+            }
+
+            String target = deletename.trim();
+            
+            deleteFile(deletename, folder);
+            currentList.remove(target);
+        }
+
+        if ( currentList.isEmpty() ){
+            return null;
+        }
+
+        return String.join(",", currentList );
+
+    } 
 
 
 }
