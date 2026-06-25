d5b2c7e2c0c7310b2f954e377ae34c233c521574
HANE48 <hun2me2ne@gmail.com>
2026-06-25 16:25:57 +0900

    06-25 한 일

--- src/main/java/com/project/foodsite/dao/ReviewDAO.java
+++ src/main/java/com/project/foodsite/dao/ReviewDAO.java
@@ -1,17 +1,11 @@
 package com.project.foodsite.dao;
 
-import java.util.List;
+import java.util.*;
 
 import com.project.foodsite.vo.ReviewVO;
 
 public interface ReviewDAO {
-
-      // 최신순
-    List<ReviewVO> reviewLatest();
-
-    // 조회수순(전체, 주간, 월간)
-    List<ReviewVO> reviewPopular(String period);
-
-    // 별점순(평점순)
-    List<ReviewVO> reviewRating();
+    List<ReviewVO> reviewLatest();                  // 최신순
+    List<ReviewVO> reviewPopular(String period);    // 조회수순(전체, 주간, 월간)
+    List<ReviewVO> reviewRating();                  // 별점순(평점순)
 }
