4288a61dd31329ec3c468aacda7c99214f73b0f1
kimhoken <kimhk441@naver.com>
2026-06-29 11:42:52 +0900

    master

--- src/main/java/com/project/foodsite/dao/ReviewDAO.java
+++ src/main/java/com/project/foodsite/dao/ReviewDAO.java
@@ -9,7 +9,7 @@ import com.project.foodsite.vo.ReviewVO;
 public interface ReviewDAO {
     List<ReviewVO> reviewLatest();                  // 최신순
     List<ReviewVO> reviewPopular(String period);    // 조회수순(전체, 주간, 월간)
-    List<ReviewVO> reviewRating();                  // 별점순(평점순)
+                   
 
     // 관리자 총 레시피 후기 갯수 조회
     int ReviewCount(AdminReviewDTO Review);
